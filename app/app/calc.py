d6612e2c629b607745c853aa83c02b02dae48fb4
Arkti <mailenaspam@gmail.com>
2024-01-12 11:18:28 +0100

    Code checks

--- app/app/calc.py
+++ app/app/calc.py
@@ -1,9 +1,12 @@
-# Calculator Function
-
+"""""
+Calculator Function
+"""
 
 def add(x , y):
+    """"Add x and y and return result. """
     return x + y
 
 
 def subtract(x, y):
+    """""Subtract x from y and return result"""
     return y - x
